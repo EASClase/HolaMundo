fa8d8e85951a2847299df35431a5b9138224df5d
EASClase <elias.aykour@educa.madrid.org>
2023-09-25 13:54:05 +0200

    version con comentarios

--- HolaMundo2/app/src/main/java/ies/barajas/holamundo/MainActivity.java
+++ HolaMundo2/app/src/main/java/ies/barajas/holamundo/MainActivity.java
@@ -2,6 +2,7 @@ package ies.barajas.holamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,25 +11,51 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
    // Button b;
     LinearLayout l;
-    Button b;
+    Button b_aceptar, b_salir, b_hola;
     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         l= findViewById(R.id.fondo);
-        b= findViewById(R.id.b_aceptar);
+        b_aceptar= findViewById(R.id.b_aceptar);
+        b_hola= findViewById(R.id.b_saludar);
+        b_salir= findViewById(R.id.b_salir);
         tv= findViewById(R.id.tv_hola);
 
         //tv.setText("Adios");
 
+        b_aceptar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onPulsado(view);
+                    }
+                }
+                /*
+                Segunda forma de utilizar un evento, con una clase anónima
+                usamos un método para instanciar un objeto en este caso de la clase onclicklistener que siempre
+                implementa el metodo onClick
+                 */
+        );
+        b_hola.setOnClickListener(this);
+        l.setOnClickListener(this);
+        tv.setOnClickListener(this);
+        /*
+        Tercera forma de utilizar un evento, implementando la interfaz onclicklistener al MainActivity, al igual que en
+        en la segunda forma, se implementa además el método onClick
+         */
+
+
     }//onCreate
 
+    public void onClick(View view) {
+        onPulsado(view);
+    }
     public void onPulsado(View view) {
         int id = view.getId();
 
